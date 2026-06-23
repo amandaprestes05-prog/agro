ded2faf0064f72cb9983fa2db72b253c85e2c905
amandaprestes05-prog <amanda.prestes05@escola.pr.gov.br>
2026-06-23 09:01:19 -0300

    scrip.js

--- scrip.js
+++ scrip.js
@@ -1,1 +1,16 @@
+function mostrarInformacoes() {
+    var caixaDeInfo = document.getElementById("info-agricultura");
+    var botao = document.querySelector(".btn-interativo");
 
+    if (caixaDeInfo.style.display === "none" || caixaDeInfo.style.display === "") {
+        caixaDeInfo.style.display = "block";
+        botao.innerHTML = "Ocultar informações"; 
+        botao.style.backgroundColor = "#E53935"; /* Muda para vermelho ao abrir */
+        botao.style.borderColor = "#B71C1C";
+    } else {
+        caixaDeInfo.style.display = "none";
+        botao.innerHTML = "Por que a Agricultura Familiar importa?"; 
+        botao.style.backgroundColor = "#4CAF50"; /* Volta para o verde */
+        botao.style.borderColor = "#388E3C";
+    }
+}
